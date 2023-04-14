605c5b07d960e28b07fa4c28ab2cb94779e121c4
Lucas Monari <lucas.monari.lm@outlook.com>
2023-04-14 19:56:16 -0300

    first Commit

--- projeto-inter/src/views/HomeView.vue
+++ projeto-inter/src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
-  <HelloWorld />
+  <v-app-bar>
+    <v-app-bar-title>
+      DevsBank
+    </v-app-bar-title>
+    <v-btn-toggle>
+      <v-btn
+        link to="/"
+
+      >
+        btn
+      </v-btn>
+      <v-btn
+        link to="/login"
+      >
+        btn
+      </v-btn>
+    </v-btn-toggle>
+  </v-app-bar>
+
+<v-main>
+  login
+</v-main>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'HomeView',
-
-  components: {
-    HelloWorld,
-  },
-});
+export default{
+  data: () => {
+    return ({
+      togglePage: 0,
+    })
+  }
+}
 </script>
---
+++ projeto-inter/src/views/LoginView.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-app-bar>
+      <v-app-bar-title>
+        DevsBank
+      </v-app-bar-title>
+      <v-btn-toggle>
+        <v-btn
+          value="0"
+          link to="/"
+
+        >
+          btn
+        </v-btn>
+        <v-btn
+          value="1"
+          link to="/login"
+
+        >
+          btn
+        </v-btn>
+      </v-btn-toggle>
+  
+    </v-app-bar>
+  <v-main>
+
+  </v-main>
+</template>
+
+  <script>
+  export default{
+    data: () => {
+      return ({
+        togglePage: 1,
+      })
+    }
+  }
+  </script>
+  

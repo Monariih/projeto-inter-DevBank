0425b0e204eb1b1c3a050c6296a0446206693b20
Lucas Monari <lucas.monari.lm@outlook.com>
2023-04-13 22:48:20 -0300

    componentes separados iniciando home

--- projeto-inter/src/components/HeaderHome.vue
+++ projeto-inter/src/components/HeaderHome.vue
@@ -1,20 +1,63 @@
 <template>
     <v-app id="inspire">
-      <v-app-bar>
+        <v-app-bar
+            id="app-bar"
+        >
+            <v-toolbar-title>
+                DevsBank <Icon icon="iconoir:dev-mode-laptop" />
+            </v-toolbar-title>
+            
+            <v-btn-toggle
+                id="btn-group"
+                v-model="text"
+                rounded="8"
+                color="yellow"
+            >
+                <v-btn value="home">
+                    HOME
+                </v-btn>
+        
+                <v-btn value="LOGIN">
+                    LOGIN
+                </v-btn>
+        
+                <v-btn value="SUPORTE">
+                    SUPORTE
+                </v-btn>
+        
+                <v-btn value="CRIAR">
+                    CRIAR CONTA
+                </v-btn>
+            </v-btn-toggle>      
+        </v-app-bar>
   
-        <v-toolbar-title>
-            DevsBank <v-icon></v-icon>
-        </v-toolbar-title>
-      </v-app-bar>
-  
-      <v-main>
+        <v-main>
         <!--  -->
-      </v-main>
+        </v-main>
     </v-app>
-  </template>
+</template>
   
-  <script>
-    export default {
-      data: () => ({ drawer: null }),
-    }
-  </script>
+<script>
+import { Icon } from '@iconify/vue';
+export default {
+    components: {
+		Icon,
+	},
+
+    data () {
+        return {
+            text: 'center',
+        }
+    },
+}
+</script>
+
+<style>
+#btn-group{
+    
+}
+#app-bar{
+    background-color: rgb(87, 14, 14);
+    color: white;
+}
+</style>
--- projeto-inter/src/views/HomeView.vue
+++ projeto-inter/src/views/HomeView.vue
@@ -4,17 +4,14 @@
 
 <script>
 import { defineComponent } from 'vue';
-
-// Components
-
 import BankHome from '@/components/BankHome.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-
     BankHome
+
   },
 });
 </script>
